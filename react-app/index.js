4c25b48f81838e5130fd40c6b3b52e4391cd4d20
Claude  Bindika <cbindika@gmail.com>
2018-05-22 22:19:24 +0200

    Adding badges for main tag words

--- react-app/index.js
+++ react-app/index.js
@@ -40,8 +40,8 @@ class Timeline extends React.Component {
                         Full stack developer with broard experience in all aspects of web development, providing client
                         consultation and support.
                         Interested in challenging development & creative opportunities.
-                        I am currently working (remote) on a Drupal 8+ReactJS membership platform with E-commerce
-                        functionalities.
+                        I am currently working remote on a <span className="badge badge-dark">Drupal 8</span> <span className="badge badge-dark">ReactJS</span> <u>membership platform</u> with <u>E-commerce
+                        functionalities</u>.
                     </p>
                 </VerticalTimelineElement>
                 <VerticalTimelineElement
@@ -54,9 +54,8 @@ class Timeline extends React.Component {
                     <h5 className="vertical-timeline-element-subtitle">London, UK</h5>
                     <h5 className="vertical-timeline-element-mydate">Feb 2016 - Dec 2017</h5>
                     <p>
-
-                        Rebuilt & developed new features for the company whole applications/websites in PHP (Drupal,
-                        Symfony). Focused primarily on the backend in addition to providing technical documentation.
+                        Rebuilt & developed new features for the company whole applications/websites in PHP <span className="badge badge-dark">Drupal</span> <span className="badge badge-dark">Symfony</span>.
+                        Focused primarily on the <u>backend</u> in addition to providing <u>technical documentation</u>.
                         <br/>
                         <ul>
                             <li>API development</li>
@@ -76,10 +75,10 @@ class Timeline extends React.Component {
                     <h5 className="vertical-timeline-element-subtitle">London, UK</h5>
                     <h5 className="vertical-timeline-element-mydate">Apr 2013 - Feb 2016</h5>
                     <p>
-                        Lead developer responsible for all digital projects in the company.
+                        Lead developer responsible for <u>all digital projects</u> in the company.
                         <br/><br/>
-                        Leading a team of 4 developers and numerous contractors on various projects (mainly
-                        Drupal/Symfony). My team has ownership of all digital deliveries (maintenance and innovation).
+                        Leading a team of 4 developers and numerous contractors on various projects mainly
+                        <span className="badge badge-dark">Drupal</span> <span className="badge badge-dark">Symfony</span>. My team has ownership of all digital deliveries (maintenance and innovation).
                         <br/><br/>
                         Required to provide process documentations and workflows, as well as manage my team and fulfil
                         my duty as a developer (see our Stackoverflow page http://bit.ly/1CNMCWc).
@@ -87,7 +86,7 @@ class Timeline extends React.Component {
                         <b>Main achievement(s):</b> Complete redesign and redevelopment of the aging main site built in Drupal
                         6/7, as well as introducing new functionalities (bloodwise.org.uk).
                         <br/><br/>
-                        Main architect of the continuous integration system involving Jenkins, Phing, Jira and Github
+                        Main architect of the <u>continuous integration</u> system involving <span className="badge badge-light">Jenkins</span> <span className="badge badge-light">Phing</span> <span className="badge badge-light">Jira</span> <span className="badge badge-light">Github</span>
                         (waffle.io). The stack is also reporting everything thanks to statuspage.io, NewRelic and some
                         custom scripts.
                     </p>
@@ -97,16 +96,15 @@ class Timeline extends React.Component {
                     iconStyle={{background: 'rgb(33, 150, 243)', color: '#fff'}}
                     icon={<WorkIcon />}
                 >
-                    <h3 className="vertical-timeline-element-title">Developer [contract]</h3>
+                    <h3 className="vertical-timeline-element-title">Developer</h3>
                     <h4 className="vertical-timeline-element-subtitle">Twogether, Creative agency -
                         https://www.wearetogether.com</h4>
                     <h5 className="vertical-timeline-element-subtitle">Beaconsfield, UK</h5>
                     <h5 className="vertical-timeline-element-mydate">Nov 2012 - Apr 2013</h5>
                     <p>
-
                         Part of a team of 2 developers, assisted by contractors (working remotely / on the project
                         basis).
-                        Building & maintaining numerous websites (Wordpress) and mainly the main application (Symfony).
+                        <u>Building & maintaining</u> numerous websites <span className="badge badge-dark">Wordpress</span> and mainly the main application <span className="badge badge-dark">Symfony</span>.
                         <br/><br/>
                         <b>Main achievement(s):</b> Main architect of various scripts to ease the day to day
                         workflow and creation of a board (using Geckoboard API) pulling in at one place all the
@@ -127,10 +125,9 @@ class Timeline extends React.Component {
                         Part of a team of 10 developers working on various projects:
                         Website creation & modification (main project: http://www.pktmny.com/,
                         http://www.agentprovocateur.com)
-                        Software development using Zend 1/2 and Backbone.js
-                        Scripting (Perl / Bash)
+                        Software development using <span className="badge badge-dark">Zend 1/2</span> & <span className="badge badge-dark">Backbone.js</span> + Scripting <span className="badge badge-dark">Perl</span> <span className="badge badge-dark">Bash</span>
                         <br/><br/>
-                        <b>Main achievement(s):</b> Lead developer of an headless back office application (build with
+                        <b>Main achievement(s):</b> Lead developer of an <u>headless back office application</u> (build with
                         Backbone JS and Zend).
                     </p>
                 </VerticalTimelineElement>
@@ -145,12 +142,12 @@ class Timeline extends React.Component {
                     <h5 className="vertical-timeline-element-subtitle">London, UK</h5>
                     <h5 className="vertical-timeline-element-mydate">Jun 2011 - Apr 2012</h5>
                     <p>
-                        Hired as a Perl developer for the integration team.
+                        Hired as a <span className="badge badge-light">Perl</span> developer for the integration team.
                         Working within a team while managing my own projects.
-                        Dedicated to the French market, providing the technical support to various stakeholders to
+                        Dedicated to the French market, providing the <u>technical support</u> to various stakeholders to
                         ensure that Broadbean's products are correctly integrated with the client's infrastructure.
                         <br/><br/>
-                        <b>Main achievement(s):</b> Creation of a set of scripts automating the workflow for the french
+                        <b>Main achievement(s):</b> Creation of a <u>set of scripts</u> automating the workflow for the french
                         market.
                     </p>
                 </VerticalTimelineElement>
@@ -165,13 +162,12 @@ class Timeline extends React.Component {
                     <h5 className="vertical-timeline-element-subtitle">Paris, France</h5>
                     <h5 className="vertical-timeline-element-subtitle">Sep 2010 - Jun 2011</h5>
                     <p>
-                        Trained developers on using and maintaining the Prestashop system (previously created and
+                        Trained developers on using and maintaining the <span className="badge badge-dark">Prestashop</span> system (previously created and
                         deployed by an agency).
                         I worked with various stakeholders to develop several modules, which, allowed the company’s
                         intranet to communicate with all their online shops.
-                        The contract was completed through the creation of a new multi site E-commerce platform
-                        (Prestashop) fully integrated with the company's intranet/websites (CRM/Custom
-                        system/CMSmadesimple/Drupal/Wordpress).
+                        The contract was completed through the creation of a new multi site <u>E-commerce platform</u> <span className="badge badge-light">Prestashop</span> fully integrated with the company's intranet/websites <span className="badge badge-light">CRM</span> <span className="badge badge-light">Custom
+                        system</span> <span className="badge badge-light">CMSmadesimple</span> <span className="badge badge-light">Drupal</span> <span className="badge badge-light">Wordpress</span>.
                     </p>
                 </VerticalTimelineElement>
                 <VerticalTimelineElement
@@ -186,12 +182,10 @@ class Timeline extends React.Component {
                     <p>
                         I was part of a 4-person team, whilst at the same time studying for my Masters.
 
-                        During the first year, I mainly worked as an HTML/CSS integrator and a PHP developer.
-                        In the second year, my responsibilities included project management and further technologies
-                        (Actionscript/AJAX/JAVA).
+                        During the first year, I mainly worked as an HTML/CSS integrator and a <span className="badge badge-dark">PHP</span> developer.
+                        In the second year, my responsibilities included project management and further technologies <span className="badge badge-light">Actionscript</span> <span className="badge badge-light">AJAX</span> <span className="badge badge-light">JAVA</span>.
 
-                        In my final year, I was responsible for the creation and maintenance of many websites in
-                        Wordpress and Drupal alongside my role in the main website of the company (Drupal 6): a high
+                        In my final year, I was responsible for the creation and maintenance of many websites in <span className="badge badge-dark">Wordpress</span> & <span className="badge badge-light">Drupal</span> alongside my role in the main website of the company <span className="badge badge-dark">Drupal 6</span>: a high
                         traffic website containing videos and flash games and a large amount of features like
                         commenting, rating and favoriting of videos and games.
                     </p>
@@ -207,9 +201,6 @@ class Timeline extends React.Component {
                         management system</h4>
                     <h5 className="vertical-timeline-element-subtitle">Paris, France</h5>
                     <h5 className="vertical-timeline-element-mydate">2009 - 2010</h5>
-                    <p>
-                        I study while working: One month at Uni the other at work
-                    </p>
                 </VerticalTimelineElement>
                 <VerticalTimelineElement
                     className="vertical-timeline-element--education"
@@ -221,9 +212,6 @@ class Timeline extends React.Component {
                         Specialized in Unix/Linux system</h4>
                     <h5 className="vertical-timeline-element-subtitle">Paris, France</h5>
                     <h5 className="vertical-timeline-element-mydate">2007 - 2009</h5>
-                    <p>
-                        I study while working: One month at Uni the other at work
-                    </p>
                 </VerticalTimelineElement>
 
                 <VerticalTimelineElement
@@ -236,9 +224,6 @@ class Timeline extends React.Component {
                         and LAN/WAN</h4>
                     <h5 className="vertical-timeline-element-subtitle">Melun, France</h5>
                     <h5 className="vertical-timeline-element-mydate">2005 - 2007</h5>
-                    <p>
-                        Study while working: One month at University the other at work
-                    </p>
                 </VerticalTimelineElement>
             </VerticalTimeline>
         )
